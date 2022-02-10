2a09a822154a0c805909f51d32c1cd760370c951
Grigroriy Lozhkin <lozhkin.grig@gmail.com>
2022-02-10 20:27:19 +0300

    Update extract_tr.py

--- scr/extract_tr.py
+++ scr/extract_tr.py
@@ -10,6 +10,6 @@ def ex_tar(t_file):
 def ex_rar(r_file, dst_folder=None):
 	mt = r_file.split(".")[0]
   if dst_folder != None:
-    mt = dst_folder
+	mt = dst_folder
 	with rarfile.RarFile(r_file) as file:
 		file.extractall(path=mt)
