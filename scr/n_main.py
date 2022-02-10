65ee23f60174ed6b31e65a6994fcbca5b531695a
Grigroriy Lozhkin <lozhkin.grig@gmail.com>
2022-02-10 23:27:50 +0300

    Update n_main.py

--- scr/n_main.py
+++ scr/n_main.py
@@ -171,3 +171,4 @@ def sta():
 
   #sew_img(512, 'membrane/membrane/im1.png', 'cop_img.png', 'membrane/membrane/g_train')
   sew_img1(512, 'membrane/membrane/im1.png', 'cop_img.png', 'membrane/membrane/g_train',3)
+  return model
