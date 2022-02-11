3d463794815c766921bf0934f2033c0d77875994
Grigroriy Lozhkin <lozhkin.grig@gmail.com>
2022-02-12 00:26:20 +0300

    Update n_main.py

--- scr/n_main.py
+++ scr/n_main.py
@@ -153,9 +153,10 @@ def sta():
                       fill_mode='nearest')'''
   
   data_gen_args = dict(rotation_range=0.2,
-                      shear_range=0.05,
-                      zoom_range=0.01,
+                      shear_range=0.2,
+                      zoom_range=0.1,
                       horizontal_flip=True,
+                      vertical_flip=True,
                       fill_mode='nearest')
   myGene = trainGenerator(2,'membrane/membrane/train','image','label',data_gen_args,save_to_dir = "membrane/membrane/t_train", target_size = (512,512))
 
