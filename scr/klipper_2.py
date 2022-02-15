074256c23b60b6cdd7786d0f519f7a54b3b8a55f
Grigroriy Lozhkin <lozhkin.grig@gmail.com>
2022-02-15 07:13:07 +0300

    Update klipper_2.py

--- scr/klipper_2.py
+++ scr/klipper_2.py
@@ -21,7 +21,8 @@ def crop_img(train_v, inp_img, out_fold, addd):
   n_num = 0       
   for file_d in os.listdir(out_fold):
           filename_d = os.fsdecode(file_d)
-          n_num = n_num + 1
+          if 'c' not in filename_d:
+              n_num = n_num + 1
         
   i_b = 0
   for i in range((height//train_v)):
